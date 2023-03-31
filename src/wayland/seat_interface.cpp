2cd703494de8194909d6d34eb309c8ce5ba20eb2
David Redondo <kde@david-redondo.de>
2023-03-31 13:52:20 +0200

    Also send drop when it was not accepted
    This enables applications to distinguish
    - successful drop
    - drop onto client that didn't accept
    - cancelled drag

--- src/wayland/seat_interface.cpp
+++ src/wayland/seat_interface.cpp
@@ -258,11 +258,18 @@ void SeatInterfacePrivate::registerPrimarySelectionDevice(PrimarySelectionDevice
 
 void SeatInterfacePrivate::cancelDrag()
 {
+
     if (drag.target) {
         drag.target->updateDragTarget(nullptr, 0);
         drag.target = nullptr;
     }
-    endDrag();
+    QObject::disconnect(drag.dragSourceDestroyConnection);
+    if (drag.source) {
+        drag.source->dndCancelled();
+    }
+    drag = Drag();
+    Q_EMIT q->dragSurfaceChanged();
+    Q_EMIT q->dragEnded();
 }
 
 void SeatInterfacePrivate::endDrag()
@@ -278,6 +285,7 @@ void SeatInterfacePrivate::endDrag()
             dragTargetDevice->drop();
             dragSource->dropPerformed();
         } else {
+            dragSource->dropPerformed();
             dragSource->dndCancelled();
         }
     }
